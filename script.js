3bc751ca1b7a6253f6f2a36f8d55dc2c59b13a86
Noled <e.morlon@codeur.online>
2019-09-12 13:59:30 +0200

    Added Marks section

--- script.js
+++ script.js
@@ -52,3 +52,26 @@ function showCommentaries(type){
     }
   }
 }
+
+/* CAROUSEL */
+$('#myCarousel').carousel({
+  interval: 10000
+})
+
+$('.carousel .carousel-item').each(function(){
+    var minPerSlide = 3;
+    var next = $(this).next();
+    if (!next.length) {
+    next = $(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
+
+    for (var i=0;i<minPerSlide;i++) {
+        next=next.next();
+        if (!next.length) {
+        	next = $(this).siblings(':first');
+      	}
+
+        next.children(':first-child').clone().appendTo($(this));
+      }
+});
